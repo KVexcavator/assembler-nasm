8b1fc20016515428182c09dd9d39a155f944c3d9
Excavator <kvronin@gmail.com>
2024-10-27 05:12:32 +0500

    update notes

--- 000_hello/hello.asm
+++ 000_hello/hello.asm
@@ -1,15 +1,15 @@
-; hello.asm
 section .data
-  msg db  "hello, world", 0
-section .bss
+  msg db  "hello, world", 0x0A  ; Добавляем символ новой строки в конец
+
 section .text
   global main
 main:
-  mov rax, 1  ; 1 = write
-  mov rdi, 1  ; 1 = to stdout
-  mov rsi, msg  ; string to display in rsi
-  mov rdx, 12 ; length of the string, without 0
-  syscall     ; display the string
-  mov rax, 60 ; 60 = exit
-  mov rdi, 0  ; 0 = success exit code
-  syscall     ; quit
+  mov rax, 1         ; 1 = syscall номер для write
+  mov rdi, 1         ; 1 = файловый дескриптор для stdout
+  mov rsi, msg       ; адрес строки для вывода
+  mov rdx, 13        ; количество байтов для вывода (12 символов + новая строка)
+  syscall            ; выводим строку
+
+  mov rax, 60        ; 60 = syscall номер для выхода
+  xor rdi, 0         ; 0 = код выхода
+  syscall            ; завершаем программу
