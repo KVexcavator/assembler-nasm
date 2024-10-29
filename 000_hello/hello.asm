5f941cfa68f98d63801d72c298fbef2e5d243aab
Excavator <kvronin@gmail.com>
2024-10-30 02:03:27 +0500

    add alive prog

--- 000_hello/hello.asm
+++ 000_hello/hello.asm
@@ -1,15 +1,20 @@
 section .data
-  msg db  "hello, world", 0x0A  ; Добавляем символ новой строки в конец
-
+  msg db  "hello, world",0
+  NL  db  0x0A  ; Добавляем символ новой строки в конец (ASCII)
+section .bss
 section .text
   global main
 main:
-  mov rax, 1         ; 1 = syscall номер для write
-  mov rdi, 1         ; 1 = файловый дескриптор для stdout
-  mov rsi, msg       ; адрес строки для вывода
-  mov rdx, 13        ; количество байтов для вывода (12 символов + новая строка)
+  mov rax, 1         ; 1 = запись
+  mov rdi, 1         ; 1 = в устройство стандартного вывода stdout
+  mov rsi, msg       ; вывводимая строка
+  mov rdx, 12        ; длина строки без завершающего 0
   syscall            ; выводим строку
-
-  mov rax, 60        ; 60 = syscall номер для выхода
-  xor rdi, 0         ; 0 = код выхода
-  syscall            ; завершаем программу
+  mov rax, 1         ; 1 = запись
+  mov rdi, 1         ; 1 = в устройство стандартного вывода stdout
+  mov rsi, NL        ; вывод символа перехода на новую строку
+  mov rdx, 1         ; длина строки
+  syscall            ; вывод строки
+  mov rax, 60        ; 60 = выход
+  mov rdi, 0         ; 0 = код выхода
+  syscall            ; выход из программы
